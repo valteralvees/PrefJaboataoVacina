180fd6d24ba6ca0b2ed0ff7919cb799bb1a6cf2b
Valter <valteralvees@outlook.com>
2022-05-10 21:17:36 -0300

    init

---
+++ PrefJaboataoVacina/src/prefjaboataovacina/Paciente.java
@@ -0,0 +1,10 @@
+package prefjaboataovacina;
+
+public class Paciente {
+    String nome;
+    long cpf;
+    boolean jaVacinado;
+    int qtdDoses;
+    int loteDose1;
+    int lotedose
+}
---
+++ PrefJaboataoVacina/src/prefjaboataovacina/PrefJaboataoVacina.java
@@ -0,0 +1,22 @@
+
+package prefjaboataovacina;
+import java.util.Scanner;
+
+public class PrefJaboataoVacina {
+
+    public static void main(String[] args) {
+        /*Dados já inseridos anteriormente que serão utilizados*/
+        Paciente p1 = new Paciente();
+        
+        
+        
+        /**/
+        
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite, por favor, o CPF do paciente:");
+        long cpfPaciente = teclado.nextLong();
+        
+        
+    }
+    
+}
